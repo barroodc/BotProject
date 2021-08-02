17724c813f752aba89b101daf27e756fc47747bf
barroodc <53627915+barroodc@users.noreply.github.com>
2021-08-02 12:58:44 -0400

    Added a print test as a result of trouble uploading this particular class.

--- src/TournamentIntro.java
+++ src/TournamentIntro.java
@@ -5,7 +5,7 @@ public class TournamentIntro {
     final static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args){
-
+        System.out.println("test");
     }
 
 }
