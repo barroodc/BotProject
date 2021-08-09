384ba5a655cbf60728fc35668d7f11388f84ac87
barroodc <53627915+barroodc@users.noreply.github.com>
2021-08-09 09:28:34 -0400

    My plan is to still iterate through using methods. So each time an incorrect answer is said I will add to the hangman. It will give around 6 tries. The tricky part is going to be tying it in to James, Ava, as well as the user.

--- src/HangMan.java
+++ src/HangMan.java
@@ -1,22 +1,59 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class HangMan {
 
     final static Scanner sc = new Scanner(System.in);
 
+        public static void hangManNoMistakes() throws IOException {
+            System.out.println("|----_----");
+            System.out.println("|    |");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|__________");
 
-    public static void hangManBoard(){
-        System.out.println("|---------");
-        System.out.println("|");
-        System.out.println("|");
+            System.out.println("Your guess: ");
+            File file = new File("/Users/christopher/Desktop/Hangman.txt");
+            Scanner scan = new Scanner(file);
+        }
+
+    public static String getRandomLineFromFile() throws IOException {
+        File file = new File("/Users/christopher/Desktop/Hangman.txt");
+        final RandomAccessFile f = new RandomAccessFile(file, "r");
+        final long randomLocation = (long) (Math.random() * f.length());
+        f.seek(randomLocation);
+        f.readLine();
+        String randomLine = f.readLine();
+        f.close();
+        return randomLine;
+        }
+
+        public static void hangManOneMistake() {
+
+            String top = ("|----_----");
+            String nooseLink1 = ("|    |");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|__________");
+
+        }
+
+
+        public static void main(String[] args) throws IOException {
+            hangManNoMistakes();
+            System.out.println(getRandomLineFromFile());
+        }
     }
 
-
-
-
-    public static void main(String[] args){
-        hangManBoard();
-    }
-
-}
