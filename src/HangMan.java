083268bcb1b634b49d9cc4c3f39c06ff15d01eb9
barroodc <53627915+barroodc@users.noreply.github.com>
2021-08-09 20:13:03 -0400

    Decided to rework this class in the hopes of making it cleaner.

--- src/HangMan.java
+++ src/HangMan.java
@@ -13,92 +13,49 @@ import java.util.stream.IntStream;
 
 public class HangMan {
 
-    //longest word is 10 letters fluffiness
-    File file = new File("/Users/christopher/Desktop/Hangman.txt");
-    Scanner words = new Scanner(file);
-    final static Scanner sc = new Scanner(System.in);
-    private static String asterisk;
-    private static int count = 0;
+    private static void actualGame() throws FileNotFoundException {
+        final Scanner scanner = new Scanner(System.in);
 
-    static {
-        try {
-            asterisk = new String(new char[getRandomLineFromFile().length()]).replace("\0", "*");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public HangMan() throws FileNotFoundException {
-    }
-
-    public static void hangManNoMistakes() throws IOException {
-            System.out.println("|----_----");
-            System.out.println("|    |");
-            System.out.println("|");
-            System.out.println("|");
-            System.out.println("|");
-            System.out.println("|");
-            System.out.println("|__________");
-
-            System.out.println("Your guess: ");
-            File file = new File("/Users/christopher/Desktop/Hangman.txt");
-            Scanner scan = new Scanner(file);
-        }
-
-    public static String getRandomLineFromFile() throws IOException {
         File file = new File("/Users/christopher/Desktop/Hangman.txt");
-        final RandomAccessFile f = new RandomAccessFile(file, "r");
-        final long randomLocation = (long) (Math.random() * f.length());
-        f.seek(randomLocation);
-        f.readLine();
-        String randomLine = f.readLine();
-        f.close();
-        return randomLine;
+        Scanner sc = new Scanner(file);
+
+        int numberIncorrect = 0;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Would you like to go first?");
+        String players = keyboard.nextLine();
+
+        if (players.equals("Yes") || players.equals("yes")){
+            System.out.println("Great lets get started!");
         }
 
-        public static void hangManOneMistake() {
+    }
 
-            System.out.println("|----_----");
-            System.out.println("|    |");
-            System.out.println("|   ( )");
-            System.out.println("|    ");
-            System.out.println("|");
-            System.out.println("|__________");
 
+    public static void main(String[] args) throws FileNotFoundException {
+
+        actualGame();
+
+    }
+
+    private static void hangingOut(int numberIncorrect){
+        System.out.println("|----_----");
+        System.out.println("|    |");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("|__________");
+
+        if (numberIncorrect >= 1){
+            System.out.println("()");
         }
 
-        public static void hangRule(String guess) throws IOException {
-            String newAsterisk = "";
-            for (int i = 0; i < getRandomLineFromFile().length(); i++){
-                if (getRandomLineFromFile().charAt(i) == guess.charAt(0)){
-                   newAsterisk += guess.charAt(0);
-                } else if (asterisk.charAt(i) != '*') newAsterisk += getRandomLineFromFile().charAt(i);
-                else {
-                    newAsterisk += "*";
-                }
-            }
-            if (asterisk.equals(newAsterisk)){
-                count++;
-                hangManNoMistakes();
-            } else {
-                asterisk = newAsterisk;
-            }
-
-            if (asterisk.equals(getRandomLineFromFile())){
-                System.out.println("Awesome! The word is " + getRandomLineFromFile());
-            }
-        }
-
-
-        public static void main(String[] args) throws IOException {
-            hangManNoMistakes();
-            while (count < 10 && asterisk.contains("*")){
-                System.out.println("Guess any letter in the word");
-                System.out.println(asterisk);
-                String guess = sc.next();
-                hangRule(guess);
-            }
-            sc.close();
+        if (numberIncorrect >=2){
+            System.out.println();
         }
     }
+
+}
 
