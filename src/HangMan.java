ab3cceb2cda06cdc18bfd0ce0cefbd50b0504f31
barroodc <53627915+barroodc@users.noreply.github.com>
2021-08-11 15:39:23 -0400

    Trying to connect two separate paths. One path will have the User go first and the other will have either James or Ava.

--- src/HangMan.java
+++ src/HangMan.java
@@ -2,20 +2,24 @@ import java.io.File;
 
 import java.io.IOException;
 
-import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
+//Have the user guess then ava then james
+//Or have james ava then user
+//Or have ava james then user
+//Tally the points at the end.
+//Bots wont guess but the user can
+
 public class HangMan {
 
     private static void introToHangman() throws IOException, InterruptedException {
-        final Scanner scanner = new Scanner(System.in);
 
         File file = new File("/Users/christopher/Desktop/Hangman.txt");
         Scanner sc = new Scanner(file);
-
-        int numberIncorrect = 0;
 
         Scanner keyboard = new Scanner(System.in);
 
@@ -24,6 +28,8 @@ public class HangMan {
 
         if (players.equals("Yes") || players.equals("yes")){
             System.out.println("Great lets get started!");
+            userGoesFirst();
+
         } else{
             System.out.println("James: I guess Ava and I will have to rock paper scissor shoot over it.");
         }
@@ -59,6 +65,8 @@ public class HangMan {
         System.out.println(ava);
 
         boolean valid = false;
+        int jamesWonRPS = 0;
+        int avaWonRPS = 0;
 
         while (!valid) {
 
@@ -68,6 +76,7 @@ public class HangMan {
                 System.out.println("Ava: Dam I guess you do");
                 TimeUnit.SECONDS.sleep(2);
                 System.out.println("James: Alright my human counterpart lets play!");
+                ++jamesWonRPS;
                 valid = true;
             } else if (james.contains("Scissor") && ava.contains("Paper")){
                 System.out.println("James: Yayyyy looks like I win!");
@@ -75,6 +84,7 @@ public class HangMan {
                 System.out.println("Ava: Dam I guess you do");
                 TimeUnit.SECONDS.sleep(2);
                 System.out.println("Alright my human counterpart lets play!");
+                ++jamesWonRPS;
                 valid = true;
             } else if (james.contains("Paper") && ava.contains("Rock")){
                 System.out.println("James: Yayyyy looks like I win!");
@@ -82,6 +92,7 @@ public class HangMan {
                 System.out.println("Ava: Dam I guess you do");
                 TimeUnit.SECONDS.sleep(2);
                 System.out.println("Alright my human counterpart lets play!");
+                ++jamesWonRPS;
                 valid = true;
             } else if (ava.contains("Rock") && james.contains("Scissor")){
                 System.out.println("Ava: Haha I knew I'd win. Not a shock really to be honest hehe :)");
@@ -89,6 +100,7 @@ public class HangMan {
                 System.out.println("James: Yeah yeah yeah good game. You won fair and square.");
                 TimeUnit.SECONDS.sleep(2);
                 System.out.println("Ava: Okay my human from another motherboard lets play some hangman!");
+                ++avaWonRPS;
                 valid = true;
             } else if (ava.contains("Scissor") && james.contains("Paper")){
                 System.out.println("Ava: Haha I knew I'd win. Not a shock really to be honest hehe :)");
@@ -96,6 +108,7 @@ public class HangMan {
                 System.out.println("James: Yeah yeah yeah good game. You won fair and square.");
                 TimeUnit.SECONDS.sleep(2);
                 System.out.println("Ava: Okay my human from another motherboard lets play some hangman!");
+                ++avaWonRPS;
                 valid = true;
             } else if (ava.contains("Paper") && james.contains("Rock")){
                 System.out.println("Ava: Haha I knew I'd win. Not a shock really to be honest hehe :)");
@@ -103,6 +116,7 @@ public class HangMan {
                 System.out.println("James: Yeah yeah yeah good game. You won fair and square.");
                 TimeUnit.SECONDS.sleep(2);
                 System.out.println("Ava: Okay my human from another motherboard lets play some hangman!");
+                ++avaWonRPS;
                 valid = true;
             } else {
                 System.out.println("James: Dam it was a tie. Lets try again.");
@@ -113,13 +127,23 @@ public class HangMan {
     }
 
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void userGoesFirst() throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(new File("/Users/christopher/Desktop/Hangman.txt"));
+        List<String> words = new ArrayList<>();
+        while (scanner.hasNext()) {
+            words.add(scanner.nextLine());
+        }
+        //Create a situation that includes yes or no to the intro.
+        //If you selected yes we can do User, then James then Ava
+        //If you selected no we can do Ava, James then User.
 
-        introToHangman();
+        System.out.println("Please guess a letter");
+        Scanner keyboard = new Scanner(System.in);
+        String letterGuess = keyboard.nextLine();
 
     }
 
-    private static void hangingOut(int numberIncorrect){
+    private static void hangingOut(Integer numberIncorrect){
         System.out.println("|----_----");
         System.out.println("|    |");
         System.out.println("|");
@@ -128,14 +152,43 @@ public class HangMan {
         System.out.println("|");
         System.out.println("|__________");
 
-        if (numberIncorrect >= 1){
-            System.out.println("()");
+        System.out.println(" -------");
+        System.out.println(" |     |");
+        if (numberIncorrect >= 1) {
+            System.out.println(" O");
         }
 
-        if (numberIncorrect >=2){
-            System.out.println("|");
+        if (numberIncorrect >= 2) {
+            System.out.print("\\ ");
+            if (numberIncorrect >= 3) {
+                System.out.println("/");
+            }
+            else {
+                System.out.println("");
+            }
         }
+
+        if (numberIncorrect >= 4) {
+            System.out.println(" |");
+        }
+
+        if (numberIncorrect >= 5) {
+            System.out.print("/ ");
+            if (numberIncorrect >= 6) {
+                System.out.println("\\");
+            }
+            else {
+                System.out.println("");
+            }
+        }
+        System.out.println("");
+        System.out.println("");
+
     }
 
+    public static void main(String[] args) throws IOException, InterruptedException{
+        introToHangman();
+        userGoesFirst();
+    }
 }
 
