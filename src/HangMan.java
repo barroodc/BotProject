d74c1b2f7c56ffc890cff0eb41695e68a63e806d
barroodc <53627915+barroodc@users.noreply.github.com>
2021-08-24 16:12:04 -0400

    Was able to make two while loops that will give james and ava points respectively. I just have implement them in a couple more classes.

--- src/HangMan.java
+++ src/HangMan.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.sql.Time;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 //Have the user guess then ava then james
 //Or have james ava then user
@@ -20,6 +22,8 @@ public class HangMan {
     static char[] lettersFromWord;
     static String letter;
     static int numberIncorrect = 0;
+    static int avasPoint = 0;
+    static int jamesPoint = 0;
 
     public static void introToHangman() throws IOException, InterruptedException {
 
@@ -50,111 +54,11 @@ public class HangMan {
         TimeUnit.SECONDS.sleep(2);
         System.out.println("James: Enough talk. Lets play");
         TimeUnit.SECONDS.sleep(2);
-        System.out.println("Agreed");
-
-        System.out.println("Ava: Would you like to go first?");
-        System.out.print("> ");
-        String players = keyboard.nextLine();
-
-        if (players.equals("Yes") || players.equals("yes")) {
-            System.out.println("Ava: Great lets get started!");
-            TimeUnit.SECONDS.sleep(2);
-            System.out.println("Ava: Make sure to enter in only lowercase letters please");
-            userGoesFirst();
-            System.exit(0);
-        } else if (players.equals("No") || players.equals("no")) {
-            System.out.println("James: I guess Ava and I will have to rock paper scissor shoot over it.");
-        }
-
-        System.out.println("James: Ready Ava?");
-        TimeUnit.SECONDS.sleep(2);
-        System.out.println("Ava: Always :)");
-        TimeUnit.SECONDS.sleep(2);
-        System.out.println("James: Awesome. Here we go!");
-        TimeUnit.SECONDS.sleep(2);
-        System.out.println("Rock");
-        TimeUnit.SECONDS.sleep(2);
-        System.out.println("Paper");
-        TimeUnit.SECONDS.sleep(2);
-        System.out.println("Scissor");
-        TimeUnit.SECONDS.sleep(2);
-        System.out.println("Shoot");
-        TimeUnit.SECONDS.sleep(2);
-
-        rockPaperScissorShoot();
+        System.out.println("Ava: Agreed");
+        userGoesFirst();
     }
 
-    private static void rockPaperScissorShoot() throws InterruptedException, IOException {
-        Random rand = new Random();
-        String[] rockPaperScissor = {"Rock", "Paper", "Scissor"};
-        String a = rockPaperScissor[rand.nextInt(rockPaperScissor.length)];
-        String b = rockPaperScissor[rand.nextInt(rockPaperScissor.length)];
 
-        String james = "James: " + a;
-        String ava = "Ava: " + b;
-
-        System.out.println(james);
-        System.out.println(ava);
-
-        boolean valid = false;
-
-        while (!valid) {
-
-            if (james.contains("Rock") && ava.contains("Scissor")) {
-                System.out.println("James: Yayyyy looks like I win!");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("Ava: Dam I guess you do");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("James: Alright my human counterpart lets play!");
-                valid = true;
-            } else if (james.contains("Scissor") && ava.contains("Paper")) {
-                System.out.println("James: Yayyyy looks like I win!");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("Ava: Dam I guess you do");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("Alright my human counterpart lets play!");
-                valid = true;
-            } else if (james.contains("Paper") && ava.contains("Rock")) {
-                System.out.println("James: Yayyyy looks like I win!");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("Ava: Dam I guess you do");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("Alright my human counterpart lets play!");
-                valid = true;
-            } else if (ava.contains("Rock") && james.contains("Scissor")) {
-                //Consider adding images to Rock Paper Scissors.
-                System.out.println("Ava: Haha I knew I'd win. Not a shock really to be honest hehe :)");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("James: Yeah yeah yeah good game. You won fair and square.");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("Ava: Okay my human from another motherboard lets play some hangman!");
-                valid = true;
-            } else if (ava.contains("Scissor") && james.contains("Paper")) {
-                System.out.println("Ava: Haha I knew I'd win. Not a shock really to be honest hehe :)");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("James: Yeah yeah yeah good game. You won fair and square.");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("Ava: Okay my human from another motherboard lets play some hangman!");
-                valid = true;
-            } else if (ava.contains("Paper") && james.contains("Rock")) {
-                System.out.println("Ava: Haha I knew I'd win. Not a shock really to be honest hehe :)");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("James: Yeah yeah yeah good game. You won fair and square.");
-                TimeUnit.SECONDS.sleep(2);
-                System.out.println("Ava: Okay my human from another motherboard lets play some hangman!");
-                valid = true;
-            } else {
-                System.out.println("James: Dam it was a tie. Lets try again.");
-                rockPaperScissorShoot();
-                break;
-            }
-        }
-    }
-
-    public static int randomNumber(int min, int max) {
-        int range = (max - min) + 1;
-        return (int) (Math.random() * range) + min;
-    }
 
     public static void numberIncorrect() {
         if (numberIncorrect == 1) {
@@ -176,6 +80,8 @@ public class HangMan {
 
     public static void userGoesFirst() throws IOException, InterruptedException {
         Scanner scanner = new Scanner(new File("/Users/christopher/Desktop/Hangman.txt"));
+        Scanner scanner2 = new Scanner(new File("/Users/christopher/Desktop/JamesWords.txt"));
+        Scanner scanner3 = new Scanner(new File("/Users/christopher/Desktop/AvasWords.txt"));
 
         List<String> words = new ArrayList<>();
         while (scanner.hasNext()) {
@@ -229,15 +135,34 @@ public class HangMan {
             if (numberIncorrect == 6){
                 System.out.println("GAME OVER");
                 System.out.println("The word was: " + word);
+
+                while (scanner2.hasNext()){
+                    String line = scanner2.nextLine();
+                    if (line.contains(word)){
+                        System.out.println("James: In your face Ava!");
+                        jamesPoint++;
+                    }
+                }
+
+                while (scanner3.hasNext()){
+                    String line = scanner3.nextLine();
+                    if (line.contains(word)){
+                        System.out.println("Ava: Hahaha one more to go.");
+                        avasPoint++;
+                    }
+                }
                 System.exit(0);
             }
         }
     }
 
+
      public static void main(String[] args) throws IOException, InterruptedException {
         introToHangman();
      }
     }
+
+
 
 
 
---
+++ src/JamesTiesAva.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class JamesTiesAva {
+    private static void rockPaperScissorShoot() throws InterruptedException, IOException {
+
+        System.out.println("Rock");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Paper");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Scissor");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Shoot");
+        TimeUnit.SECONDS.sleep(2);
+        Random rand = new Random();
+        String[] rockPaperScissor = {"Rock", "Paper", "Scissor"};
+        String a = rockPaperScissor[rand.nextInt(rockPaperScissor.length)];
+        String b = rockPaperScissor[rand.nextInt(rockPaperScissor.length)];
+
+        String james = "James: " + a;
+        String ava = "Ava: " + b;
+
+        System.out.println(james);
+        System.out.println(ava);
+
+        boolean valid = false;
+
+        while (!valid) {
+
+            if (james.contains("Rock") && ava.contains("Scissor")) {
+                System.out.println("James: Yayyyy looks like I win!");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("Ava: Dam I guess you do");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("James: Alright my human counterpart lets play!");
+                valid = true;
+            } else if (james.contains("Scissor") && ava.contains("Paper")) {
+                System.out.println("James: Yayyyy looks like I win!");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("Ava: Dam I guess you do");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("Alright my human counterpart lets play!");
+                valid = true;
+            } else if (james.contains("Paper") && ava.contains("Rock")) {
+                System.out.println("James: Yayyyy looks like I win!");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("Ava: Dam I guess you do");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("Alright my human counterpart lets play!");
+                valid = true;
+            } else if (ava.contains("Rock") && james.contains("Scissor")) {
+                //Consider adding images to Rock Paper Scissors.
+                System.out.println("Ava: Haha I knew I'd win. Not a shock really to be honest hehe :)");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("James: Yeah yeah yeah good game. You won fair and square.");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("Ava: Okay my human from another motherboard lets play some hangman!");
+                valid = true;
+            } else if (ava.contains("Scissor") && james.contains("Paper")) {
+                System.out.println("Ava: Haha I knew I'd win. Not a shock really to be honest hehe :)");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("James: Yeah yeah yeah good game. You won fair and square.");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("Ava: Okay my human from another motherboard lets play some hangman!");
+                valid = true;
+            } else if (ava.contains("Paper") && james.contains("Rock")) {
+                System.out.println("Ava: Haha I knew I'd win. Not a shock really to be honest hehe :)");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("James: Yeah yeah yeah good game. You won fair and square.");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("Ava: Okay my human from another motherboard lets play some hangman!");
+                valid = true;
+            } else {
+                System.out.println("James: Dam it was a tie. Lets try again.");
+                rockPaperScissorShoot();
+                break;
+            }
+        }
+    }
+}
