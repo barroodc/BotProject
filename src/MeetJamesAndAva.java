8af583ceaedb158b9e828e15233937f9d356cf03
barroodc <53627915+barroodc@users.noreply.github.com>
2021-08-27 20:11:32 -0400

    Decided to implement a class where I have a title screen. Thinking about linking it to the rest of everything.

---
+++ src/MeetJamesAndAva.java
@@ -0,0 +1,12 @@
+public class MeetJamesAndAva {
+
+    public static void main(String[] args) {
+
+
+
+
+        System.out.println("*******************    ********************");
+        System.out.println("*                 *    *                  *");
+        System.out.println("* (  0  ) (  0  ) *    * (  0   )  ( 0 )  *");
+    }
+}
